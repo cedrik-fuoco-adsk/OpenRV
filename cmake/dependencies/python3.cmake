f69fee43018311cd91722681544baeb51a59cf96
Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>
2025-07-14 11:50:57 -0400

    Update python3.cmake
    Signed-off-by: Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>

--- cmake/dependencies/python3.cmake
+++ cmake/dependencies/python3.cmake
@@ -8,6 +8,42 @@
 # dependency on a separate OpenSSL build, and speeds up dependency resolution.
 #
 
+# Get all propreties that cmake supports
+if(NOT CMAKE_PROPERTY_LIST)
+    execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERTY_LIST)
+    
+    # Convert command output into a CMake list
+    string(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
+    string(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
+    list(REMOVE_DUPLICATES CMAKE_PROPERTY_LIST)
+endif()
+    
+function(print_properties)
+    message("CMAKE_PROPERTY_LIST = ${CMAKE_PROPERTY_LIST}")
+endfunction()
+    
+function(print_target_properties target)
+    if(NOT TARGET ${target})
+      message(STATUS "There is no target named '${target}'")
+      return()
+    endif()
+
+    foreach(property ${CMAKE_PROPERTY_LIST})
+        string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" property ${property})
+
+        # Fix https://stackoverflow.com/questions/32197663/how-can-i-remove-the-the-location-property-may-not-be-read-from-target-error-i
+        if(property STREQUAL "LOCATION" OR property MATCHES "^LOCATION_" OR property MATCHES "_LOCATION$")
+            continue()
+        endif()
+
+        get_property(was_set TARGET ${target} PROPERTY ${property} SET)
+        if(was_set)
+            get_target_property(value ${target} ${property})
+            message("${target} ${property} = ${value}")
+        endif()
+    endforeach()
+endfunction()
+
 SET(_python3_target
     "RV_DEPS_PYTHON3"
 )
@@ -494,6 +530,8 @@ LIST(APPEND RV_DEPS_LIST Python::Python)
 
 ADD_DEPENDENCIES(dependencies ${_python3_target}-stage-target)
 
+print_target_properties(Python::Python)
+message(STATUS "cedrik ${_include_dir}")
 SET(RV_DEPS_PYTHON3_VERSION
     ${_python3_version}
     CACHE INTERNAL "" FORCE
