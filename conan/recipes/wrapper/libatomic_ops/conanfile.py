0c99d20e153d5fa098e82536241b40d62c9121c0
Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>
2025-07-24 10:52:55 -0400

    Update conanfile.py
    Signed-off-by: Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>

--- conan/recipes/wrapper/libatomic_ops/conanfile.py
+++ conan/recipes/wrapper/libatomic_ops/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.files import load, save
+from conan.tools.files import load, save, copy
 import os
 
 class Libatomic_opsWrapper(ConanFile):
@@ -8,9 +8,16 @@ class Libatomic_opsWrapper(ConanFile):
     requires = "libatomic_ops/7.8.2"
 
     def export_sources(self):
-        self.copy("conandata.yml")
+        # Copy conandata.yml from the Conan Center recipe's cache folder
+        print("cedrik")
+        print(self.dependencies)
+        dep = self.dependencies["libatomic_ops/7.8.2"]
+        conandata_src = os.path.join(dep.recipe_folder, "conandata.yml")
+        conandata_dst = os.path.join(self.recipe_folder, "conandata.yml")
+        copy(self, "conandata.yml", src=dep.recipe_folder, dst=self.recipe_folder)
 
     def source(self):
+        # Load and modify conandata.yml
         conandata_path = os.path.join(self.recipe_folder, "conandata.yml")
         if os.path.exists(conandata_path):
             conandata = load(self, conandata_path)
@@ -20,4 +27,5 @@ class Libatomic_opsWrapper(ConanFile):
                 data["sources"][self.version]["url"] = "https://github.com/bdwgc/libatomic_ops/releases/download/v7.8.2/libatomic_ops-7.8.2.tar.gz"
                 data["sources"][self.version]["sha256"] = "d305207fe207f2b3fb5cb4c019da12b44ce3fcbc593dfd5080d867b1a2419b51"
             save(self, conandata_path, yaml.safe_dump(data))
+        # Call the original source method
         super().source()
