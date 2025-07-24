b251e5b3d28bb117aff3ac451ae15198701ce594
Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>
2025-07-24 04:49:39 -0400

    Update hook_atomic_ops_source.py
    Signed-off-by: Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>

--- conan/extensions/hooks/hook_atomic_ops_source.py
+++ conan/extensions/hooks/hook_atomic_ops_source.py
@@ -2,7 +2,8 @@ from conan.tools.files import load, save
 import os
 
 def pre_source(conanfile):
-    print("hello from cedrik")
+    print("hello from cedrik", conanfile.name, conanfile.version)
+    print("cedrik: ", conanfile.recipe_folder)
     # Check if the recipe is the one you want to modify
     if conanfile.name == "libatomic_ops" and conanfile.version == "7.8.2":
         # Load the conandata.yml
